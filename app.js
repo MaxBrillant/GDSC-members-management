c43e4758dd71d11414a86473c36d558f5f000be8
Max <ndashimax37@gmail.com>
2023-05-20 22:43:31 +0300

    Adding the popup form and customizing some input elements

--- app.js
+++ app.js
@@ -1,10 +1,85 @@
+// Action button for each member should
+// have a actions button, and when it is clicked we want to show
+//  the dropdown with the buttons "edit" and "delete"
+
 let actions = document.querySelector('.actions');
 let dropdown = document.querySelector('.dropdown');
 
+//when the actions button is clicked, we want to show the actions in the dropdown
 actions.addEventListener('click', function (event) {
     event.stopPropagation();
     dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
 });
-document.addEventListener('click', function () {
+//if the actions button has been clicked, anything else that is clicked will 
+// make the dropdown disappear
+document.addEventListener('click', () => {
     dropdown.style.display = 'none';
+});
+
+
+
+//We want to deal with the opening and closing of the "new member" popup.
+let popupBackground = document.querySelector(".popup-background");
+let closeButton = document.querySelector(".close-popup");
+let addButton = document.querySelector(".addMember");
+
+popupBackground.addEventListener('click', (event) => {
+    if(event.target === popupBackground){
+        popupBackground.style.display = 'none';
+    }
+})
+closeButton.addEventListener('click', () => {
+    popupBackground.style.display = 'none';
+})
+addButton.addEventListener('click', () => {
+    popupBackground.style.display = 'block';
+})
+
+
+
+
+//Each time we select a programming language ,
+//we want to add it to our programming list
+
+let languageSelect = document.querySelector(".language-select");
+let languageList = document.querySelector(".language-list");
+
+languageSelect.addEventListener('change', (event)=> {
+
+    //if the selected option is not the placeholder value
+    if(event.target.value !== languageSelect.firstElementChild.value) {
+        
+        //We are going to create a new language element that has a name and a closing button
+        let language = document.createElement('div');
+        language.className = 'language';
+
+        let name = document.createElement('span');
+        
+        const selectedIndex = languageSelect.selectedIndex;
+        const selectedText = languageSelect.options[selectedIndex].text;
+        name.innerHTML = selectedText;
+        language.appendChild(name);
+
+        let closeButton = document.createElement('button');
+        closeButton.className = 'btn-close';
+        language.appendChild(closeButton);
+
+
+        languageList.appendChild(language);
+
+        //Each time we add language to the list, we want to remove it from the language select
+        languageSelect.remove(selectedIndex);
+
+
+        //When we click on the close button of a languange that is already on the list, 
+        //we will remove it from the list and add it back to the language select element.
+        closeButton.addEventListener('click', (event) => {
+            const maxIndex = languageSelect.options.length;
+            let newOption = new Option(name.innerHTML, "'"+maxIndex+"'");
+            languageSelect.add(newOption);
+            languageList.removeChild(event.target.parentElement);
+        });
+
+
+    }
 });
